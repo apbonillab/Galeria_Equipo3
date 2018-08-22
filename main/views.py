597e2415124e05dd0cb9ddfa38b3ff9fb1a66f53
Adriana Bonilla <apbonillab@gmail.com>
2018-08-22 16:40:49 -0500

    gitignore pyc add boostrap

--- main/views.py
+++ main/views.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-'''
+
+
 from django.shortcuts import render
 from django.shortcuts import render
-from .models import Foto
 # Create your views here.
 def index(request):
-    lista_imagenes = Foto.objects.all();
-    context = {'lista_imagenes' : lista_imagenes}
+
+    context = {'lista_imagenes' : 'fllala'}
     return render(request,'main/index.html',context)
-'''
+
